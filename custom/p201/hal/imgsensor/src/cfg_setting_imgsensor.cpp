f46f1ee4602951d60be5ce11c051a49358bb644c
huangchao <huangchao@shizhongkeji.com>
2015-07-28 10:58:28 +0800

    摄像头旋转还原

--- custom/p201/hal/imgsensor/src/cfg_setting_imgsensor.cpp
+++ custom/p201/hal/imgsensor/src/cfg_setting_imgsensor.cpp
@@ -11,7 +11,7 @@ SensorOrientation_T const&
 getSensorOrientation()
 {
     static SensorOrientation_T const inst = {	 // p201kk2\mediatek\custom\p201\hal\imgsensor\src\cfg_setting_imgsensor.cpp
-        u4Degree_0  : 180,//90,   //  main sensor in degree (0, 90, 180, 270)
+        u4Degree_0  : 90,   //  main sensor in degree (0, 90, 180, 270)
         u4Degree_1  : 270,    //  sub  sensor in degree (0, 90, 180, 270)
         u4Degree_2  : 90,   //  main2 sensor in degree (0, 90, 180, 270) 
     };
